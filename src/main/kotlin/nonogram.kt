b8e5951e65ef4bf54cb0743e49e49e51e22d088d
Allegra <ATPanetta18@gmail.com>
2020-12-20 14:20:48 -0500

    move to new repo

---
+++ src/main/kotlin/nonogram.kt
@@ -0,0 +1,234 @@
+import kotlin.properties.Delegates
+
+/**
+ * A Puzzle is a list of [Hint]s for the columns and rows.
+ * @property colHints list of hints for columns from left to right
+ * @property rowHints list of hints for rows from top to bottom
+ * @property numCols the number of columns
+ * @property numRows the number of rows
+ * @property board the [Board] for this Puzzle
+ * @property lines the [Line]s derived from the [Hint]s and [Board]
+ */
+class Puzzle(val colHints: List<Hint>, val rowHints: List<Hint>) {
+    val numCols = colHints.size
+    val numRows = rowHints.size
+
+    val board = Board(numCols, numRows)
+
+    val lines by lazy {
+        val mLines = mutableListOf<Line>()
+        for ((index, hint) in colHints.withIndex()) {
+            val squares = mutableListOf<Square>()
+            for (row in 0 until numRows) {
+                board[index, row]?.let { squares.add(it) }
+            }
+            mLines.add(Line(hint, squares.toList()))
+        }
+        for ((index, hint) in rowHints.withIndex()) {
+            val squares = mutableListOf<Square>()
+            for (col in 0 until numCols) {
+                board[col, index]?.let { squares.add(it) }
+            }
+            mLines.add(Line(hint, squares.toList()))
+        }
+        mLines.toList()
+    }
+
+    /** Checks if the current [Board] solves the puzzle */
+    fun isSolved(): Boolean {
+        for (line in lines) {
+            if (line.getBlockSizes() != line.hint.values) return false
+        }
+        return true
+    }
+}
+
+/**
+ * A Line is a column or row of [Square]s and the corresponding [Hint].
+ * Access the [Square]s using the indexed access operator (square brackets).
+ * @property hint the [Hint] that restricts the line
+ * @property squares the [Square]s that make up this line
+ * @property blocks the current [Block]s in this line
+ */
+class Line(val hint: Hint, val squares: List<Square>) : SquareObserver {
+    lateinit var blocks: List<Block>
+
+    init {
+        squares.forEach { it.observers.add(this) }
+        update()
+    }
+
+    /** Computes the current [Block]s in the line */
+    override fun update() {
+        var blockSize = 0
+        var blockStart = 0
+        val mBlocks = mutableListOf<Block>()
+        var lastState = State.EMPTY
+        for ((index, square) in squares.withIndex()) {
+            if (lastState != State.FILLED) { // looking for block
+                if (square.state == State.FILLED) {
+                    blockSize = 1
+                    blockStart = index
+                }
+            } else { // building a block
+                if (square.state == State.FILLED) blockSize++
+                else {
+                    mBlocks.add(Block(blockStart, blockSize))
+                    blockSize = 0
+                }
+            }
+            lastState = square.state
+        }
+        if (blockSize > 0) mBlocks.add(Block(blockStart, blockSize))
+        blocks = mBlocks.toList()
+    }
+
+    /** Gets the current sizes of blocks in the line */
+    fun getBlockSizes(): List<Int> {
+        val sizes = mutableListOf<Int>()
+        blocks.forEach { sizes.add(it.size) }
+        return sizes.toList()
+    }
+
+    /** Gets the current starting indices of blocks in the line */
+    fun getBlockStarts(): List<Int> {
+        val starts = mutableListOf<Int>()
+        blocks.forEach { starts.add(it.startIndex) }
+        return starts.toList()
+    }
+
+    /** Checks if the [Line] contains a [Square] with the given [State]. */
+    fun contains(state: State): Boolean {
+        for (square in squares) if (square.state == state) return true
+        return false
+    }
+
+    /** Set the [State] of a [Square] in this [Line] by index. */
+    operator fun set(index: Int, newState: State) {
+        squares[index].state = newState
+    }
+
+    /** Access the [Square]s in this [Line] by index. */
+    operator fun get(index: Int): Square {
+        return squares[index]
+    }
+}
+
+/**
+ * A Block is a group of adjacent Squares.
+ * @property startIndex the starting location of this block in the line
+ * @property size the length of the block
+ */
+data class Block(val startIndex: Int, val size: Int)
+
+/**
+ * A Hint is a list of integers representing the block sizes in the column/row.
+ * Access the list with the indexed access operator (square brackets).
+ * @property values the list of block sizes
+ * @property size minimum size of the blocks and spaces
+ */
+class Hint(val values: List<Int>) {
+    val size = values.size - 1 + values.sum()
+
+    /**
+     * The [Hint]'s size is used for comparison.
+     * @return a negative number if this [Hint]'s size is less than the other
+     */
+    operator fun compareTo(other: Hint): Int {
+        return this.size - other.size
+    }
+
+    /** Access the [Hint]'s values by index. */
+    operator fun get(index: Int): Int {
+        return values[index]
+    }
+}
+
+/**
+ * The Board is a map of locations as Pair(column, row) to [Square].
+ * Access the map with the indexed access operator (square brackets).
+ * @property numCols the number of columns in the board
+ * @property numRows the number of rows in the board
+ * @constructor creates a blank ([State].UNKNOWN) board of the given size
+ */
+class Board(val numCols: Int, val numRows: Int) {
+    private val map by lazy {
+        val mMap = mutableMapOf<Pair<Int, Int>, Square>()
+        for (c in 0 until numCols) {
+            for (r in 0 until numRows) {
+                mMap[Pair(c, r)] = Square()
+            }
+        }
+        mMap.toMap()
+    }
+
+    /** Checks if the [Board] contains a [Square] with the given [State]. */
+    fun contains(state: State): Boolean {
+        for ((_, square) in map) if (square.state == state) return true
+        return false
+    }
+
+    /** Change the [State] of a [Square] given its location as a Pair. */
+    operator fun set(location: Pair<Int, Int>, newState: State) {
+        map[location]?.state = newState
+    }
+
+    /** Change the [State] of a [Square] given its column and row. */
+    operator fun set(column: Int, row: Int, newState: State) {
+        map[Pair(column, row)]?.state = newState
+    }
+
+    /** Access the [Square]s with a location Pair. */
+    operator fun get(location: Pair<Int, Int>): Square? {
+        return map[location]
+    }
+
+    /** Access the [Square]s with a column and row. */
+    operator fun get(column: Int, row: Int): Square? {
+        return map[Pair(column, row)]
+    }
+
+    /**
+     * Does a deep comparison by the [State]s of each [Board]'s [Square]s.
+     * @return true if the corresponding [Square]s on each [Board]
+     * have the same [State], false otherwise
+     */
+    fun sameStates(other: Board): Boolean {
+        if (other.numCols != this.numCols ||
+            other.numRows != this.numRows
+        ) {
+            return false
+        }
+        for (col in 0 until numCols) {
+            for (row in 0 until numRows) {
+                if (other[col, row]?.state != this[col, row]?.state) return false
+            }
+        }
+        return true
+    }
+}
+
+/**
+ * A single space on the [Board].
+ * @property observers objects to notify when the state changes
+ * @property state the current [State] of this space
+ */
+class Square {
+    val observers = mutableSetOf<SquareObserver>()
+    var state by Delegates.observable(State.UNKNOWN) { _, _, _ ->
+        observers.forEach { it.update() }
+    }
+}
+
+/**
+ * Classes that want to update when a [Square]'s [State] changes
+ * can implement this interface and register with the [Square]s.
+ */
+interface SquareObserver {
+    fun update()
+}
+
+/** The possible states of a [Square] on the [Board]: UNKNOWN, EMPTY, FILLED */
+enum class State {
+    UNKNOWN, EMPTY, FILLED
+}
---
+++ src/test/kotlin/ModelTests.kt
@@ -0,0 +1,225 @@
+import org.junit.jupiter.api.*
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ModelTest {
+    @Nested
+    inner class SquareTests {
+        private val square1 = Square()
+
+        @Test
+        fun `initialized to UNKNOWN`() {
+            assertEquals(State.UNKNOWN, square1.state)
+        }
+
+        @Test
+        fun `change state`() {
+            square1.state = State.EMPTY
+            assertEquals(State.EMPTY, square1.state)
+            square1.state = State.FILLED
+            assertEquals(State.FILLED, square1.state)
+        }
+    }
+
+    @Nested
+    inner class BoardTests {
+        @Test
+        fun `test Board State`() {
+            val board = Board(1, 1)
+            assertEquals(State.UNKNOWN, board[Pair(0, 0)]!!.state)
+            board[Pair(0, 0)] = State.EMPTY
+            assertEquals(State.EMPTY, board[0, 0]!!.state)
+            board[0, 0] = State.FILLED
+            assertEquals(State.FILLED, board[0, 0]!!.state)
+        }
+
+        @Test
+        fun `test Board equality`() {
+            val board1 = Board(3, 3)
+            val board2 = Board(3, 3)
+            val board3 = Board(3, 4)
+            board1[0, 0] = State.EMPTY
+            board1[1, 1] = State.FILLED
+            board2[0, 0] = State.EMPTY
+            board2[1, 1] = State.FILLED
+            board3[0, 0] = State.EMPTY
+            board3[1, 1] = State.FILLED
+            assertTrue(board1.sameStates(board2))
+            assertTrue(board2.sameStates(board1))
+            assertFalse(board2.sameStates(board3))
+            assertFalse(board3.sameStates(board2))
+            board1[2, 2] = State.FILLED
+            assertFalse(board1.sameStates(board2))
+        }
+
+        @Test
+        fun `test contains`() {
+            val board1 = Board(3, 3)
+            assertTrue(board1.contains(State.UNKNOWN))
+            assertFalse(board1.contains(State.FILLED))
+            assertFalse(board1.contains(State.EMPTY))
+            board1[0, 0] = State.EMPTY
+            board1[2, 2] = State.FILLED
+            assertTrue(board1.contains(State.FILLED))
+            assertTrue(board1.contains(State.EMPTY))
+        }
+    }
+
+    @Nested
+    inner class HintTests {
+        private val hint1 = Hint(listOf(2, 5))
+        private val hint2 = Hint(listOf(3, 5))
+        private val hint3 = Hint(listOf(1, 2, 3))
+
+        @Test
+        fun `test constructor`() {
+            assertEquals(5, hint1[1])
+            assertEquals(9, hint2.size)
+        }
+
+        @Test
+        fun `test comparison`() {
+            assertTrue(hint1 < hint2)
+            assertTrue(hint1 <= hint3)
+        }
+    }
+
+    @Nested
+    inner class BlockTests {
+        private val board1 = Board(4, 3)
+        private val hint1 = Hint(listOf(1, 2))
+        private val line1 = Line(hint1, listOf(board1[0, 0]!!, board1[0, 1]!!, board1[0, 2]!!))
+
+        @Test
+        fun `test constructor`() {
+            line1[0] = State.FILLED
+            line1[1] = State.FILLED
+            line1[2] = State.FILLED
+            assertDoesNotThrow { Block(0, 3) }
+        }
+    }
+
+    @Nested
+    inner class LineTests {
+        private val board1 = Board(4, 3)
+        private val hint1 = Hint(listOf(1, 2))
+        private val line1 = Line(hint1, listOf(board1[0, 0]!!, board1[0, 1]!!, board1[0, 2]!!))
+
+        @Test
+        fun `test indexed access`() {
+            line1[1] = State.EMPTY
+            assertEquals(State.EMPTY, line1[1].state)
+        }
+
+        @Test
+        fun `test hint access`() {
+            assertEquals(line1.hint[1], 2)
+        }
+
+        @Test
+        fun `test contains`() {
+            assertFalse(line1.contains(State.FILLED))
+            line1.squares[2].state = State.FILLED
+            assertTrue(line1.contains(State.FILLED))
+        }
+
+        @Test
+        fun `test block starts`() {
+            line1[0] = State.FILLED
+            line1[1] = State.EMPTY
+            line1[2] = State.FILLED
+            assertEquals(listOf(0, 2), line1.getBlockStarts())
+        }
+
+        @Test
+        fun `test getBlocks`() {
+            line1[0] = State.FILLED
+            line1[1] = State.EMPTY
+            line1[2] = State.FILLED
+            val blocks = line1.blocks
+            assertEquals(2, blocks.size)
+            assertEquals(0, blocks[0].startIndex)
+            assertEquals(2, blocks[1].startIndex)
+            assertEquals(1, blocks[1].size)
+        }
+    }
+
+    @Nested
+    inner class PuzzleTests {
+        private val testPuzzle by lazy {
+            val hintCol1 = Hint(listOf(1))
+            val hintCol2 = Hint(listOf(5))
+            val hintCol3 = Hint(listOf(2))
+            val hintCol4 = Hint(listOf(4))
+            val hintCol5 = Hint(listOf(2, 1))
+            val hintCol6 = Hint(listOf(2))
+            val colHints = listOf(hintCol1, hintCol2, hintCol3, hintCol4, hintCol5, hintCol6)
+            val hintRow1 = Hint(listOf(2, 1))
+            val hintRow2 = Hint(listOf(1, 3))
+            val hintRow3 = Hint(listOf(1, 2))
+            val hintRow4 = Hint(listOf(3))
+            val hintRow5 = Hint(listOf(4))
+            val rowHints = listOf(hintRow1, hintRow2, hintRow3, hintRow4, hintRow5)
+            val puzzle = Puzzle(colHints, rowHints)
+            puzzle.board[0, 0] = State.FILLED
+            puzzle.board[0, 1] = State.EMPTY
+            puzzle.board[0, 2] = State.EMPTY
+            puzzle.board[0, 3] = State.EMPTY
+            puzzle.board[0, 4] = State.EMPTY
+            puzzle.board[1, 0] = State.FILLED
+            puzzle.board[1, 1] = State.FILLED
+            puzzle.board[1, 2] = State.FILLED
+            puzzle.board[1, 3] = State.FILLED
+            puzzle.board[1, 4] = State.FILLED
+            puzzle.board[2, 0] = State.EMPTY
+            puzzle.board[2, 1] = State.EMPTY
+            puzzle.board[2, 2] = State.EMPTY
+            puzzle.board[2, 3] = State.FILLED
+            puzzle.board[2, 4] = State.FILLED
+            puzzle.board[3, 0] = State.EMPTY
+            puzzle.board[3, 1] = State.FILLED
+            puzzle.board[3, 2] = State.FILLED
+            puzzle.board[3, 3] = State.FILLED
+            puzzle.board[3, 4] = State.FILLED
+            puzzle.board[4, 0] = State.EMPTY
+            puzzle.board[4, 1] = State.FILLED
+            puzzle.board[4, 2] = State.FILLED
+            puzzle.board[4, 3] = State.EMPTY
+            puzzle.board[4, 4] = State.FILLED
+            puzzle.board[5, 0] = State.FILLED
+            puzzle.board[5, 1] = State.FILLED
+            puzzle.board[5, 2] = State.EMPTY
+            puzzle.board[5, 3] = State.EMPTY
+            puzzle.board[5, 4] = State.EMPTY
+            puzzle
+        }
+
+        @Test
+        fun `test puzzle size`() {
+            assertEquals(6, testPuzzle.numCols)
+            assertEquals(5, testPuzzle.numRows)
+            assertEquals(6, testPuzzle.colHints.size)
+            assertEquals(5, testPuzzle.rowHints.size)
+        }
+
+        @Test
+        fun `test line parsing`() {
+            assertEquals(5, testPuzzle.lines[1].hint[0])
+        }
+
+        @Test
+        fun `test isSolved`() {
+            assertTrue(testPuzzle.isSolved())
+            for (col in 0 until testPuzzle.numCols) {
+                for (row in 0 until testPuzzle.numRows) {
+                    testPuzzle.board[col, row] = State.FILLED
+                }
+            }
+            assertFalse(testPuzzle.isSolved())
+            testPuzzle.board[0, 0] = State.UNKNOWN
+            assertFalse(testPuzzle.isSolved())
+        }
+    }
+}
